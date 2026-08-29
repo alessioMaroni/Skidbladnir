bd9dede3599a8c6969fd359ae0d11ce84353cf4b
alessioMaroni <alessioMaroni0@gmail.com>
2026-08-29 11:08:48 +0200

    feat(kernel panic): Printing the panic info using macro println!

--- src/panic.rs
+++ src/panic.rs
@@ -7,10 +7,9 @@
 // except according to those terms.
 
 #![cfg(not(test))]
-#[allow(unused_variables)]
 #[panic_handler]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
-    // TODO: Once io module is implemented print the panic info
+    crate::println!("[Kernel Panics] Panic: {:?}", info);
     loop {
 
     }
